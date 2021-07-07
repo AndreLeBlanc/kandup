0a5a13fa100faf08054cc3d30db650eb8ff57060
AndreLeBlanc <leblanc_112@hotmail.com>
2021-07-07 11:49:06 +0200

    removed c0 bias for alexnet

--- cnnGabZern.py
+++ cnnGabZern.py
@@ -85,7 +85,7 @@ class AlexNet(nn.Module):
     def __init__(self, fType, global_params=None):
         super(AlexNet, self).__init__()
         self.fType = fType
-        self.c0 = nn.Conv2d(3, 64, kernel_size=11, stride=4)
+        self.c0 = nn.Conv2d(3, 64, kernel_size=11, stride=4, bias=False)
         self.z0 = Zernike(3, 64, kernel_size=11, stride=4)
         self.g0 = GaborConv2d(3, 64, kernel_size=11, stride=4)
 
